e58c9dd59beccf64a03179260df7b89daa6aba06
Arity-T <tish.mainmail@gmail.com>
2024-10-24 20:21:15 +0300

    Заготовка для класса

---
+++ quaternion/quaternion.py
@@ -0,0 +1,30 @@
+__all__ = ["Quaternion"]
+
+
+class Quaternion:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    def inverse(self):
+        pass
+
+    def __truediv__(self, other):
+        pass
+
+    def conjugate(self):
+        pass
+
+    def norm(self):
+        pass
