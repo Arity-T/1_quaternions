fd68f4e9f7f28e76f8a2999210654330873e4c0e
дмитрий губковский <gubkovskij.dv@edu.spbstu.ru>
2024-10-26 19:44:37 +0300

    Добавил тест при деление на 0.

--- tests/test_quaternion_division.py
+++ tests/test_quaternion_division.py
@@ -9,3 +9,13 @@ def test_quaternion_division() -> None:
     assert result.b == -1.25
     assert result.c == 3.75
     assert result.d == 1.25
+
+def test_quaternion_division_by_zero() -> None:
+    q1 = Quaternion(10, 10, 20, 30)
+    q2 = Quaternion(0, 0, 0, 0)
+    result = q1 / q2
+
+    assert result == None
+
+
+
