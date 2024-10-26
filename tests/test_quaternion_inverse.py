bd2ccdd9c48da1a22d01cd349d0c53de1660828e
Vladislav Gaar <gaarvlad@gmail.com>
2024-10-26 21:29:13 +0300

    Added tests for Quaternion's inverse method

---
+++ tests/test_quaternion_inverse.py
@@ -0,0 +1,42 @@
+from quaternion import Quaternion
+
+def test_quaternion_inverse() -> None:
+    q = Quaternion(1,2,3,4)
+    q_inv = q.inverse()
+    inentity_approx = q * q_inv # Approximately unit quaternion
+    assert (
+        abs(identity_approx.a - 1) < 1e-6 and
+        abs(identity_approx.b) < 1e-6 and
+        abs(identity_approx.c) < 1e-6 and
+        abs(identity_approx.d) < 1e-6
+    )
+
+def test_quaternion_inverse_zero() -> None:
+    try:
+        q_zero = Quaternion(0, 0, 0, 0)
+        q_zero.inverse()
+        assert False
+    except ZeroDivisionError: # Except expected, test passed
+        pass
+
+def test_quaternion_inverse_unit() -> None:
+    q = Quaternion(1,0,0,0)
+    q_inv = q.inverse()
+    assert (
+        abs(q_inv.a - q.a) < 1e-6 and
+        abs(q_inv.b - q.b) < 1e-6 and
+        abs(q_inv.c - q.c) < 1e-6 and
+        abs(q_inv.d - q.d) < 1e-6
+    )
+
+
+def test_quaternion_inverse_negative() -> None:
+    q = Quaternion(-1,-2,-3,-4)
+    q_inv = q.inverse()
+    identity_approx = q * q_inv
+    assert (
+        abs(identity_approx.a - 1) < 1e-6 and
+        abs(identity_approx.b) < 1e-6 and
+        abs(identity_approx.c) < 1e-6 and
+        abs(identity_approx.d) < 1e-6
+    )
